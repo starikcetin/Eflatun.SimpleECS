b595dff8144b661d34cc2545d4dbbcd3dcd79990
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-28 18:16:01 +0300

    Created projects

---
+++ SimpleECS/Development/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Development
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
