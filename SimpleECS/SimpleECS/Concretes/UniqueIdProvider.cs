4c4ec001dc86d47d72111cd446cc337bd72c6d8d
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-29 14:12:40 +0300

    UniqueIdProvider implemented

---
+++ SimpleECS/SimpleECS/Concretes/UniqueIdProvider.cs
@@ -0,0 +1,14 @@
+﻿using SimpleECS.Interfaces;
+
+namespace SimpleECS.Concretes
+{
+    public class UniqueIdProvider : IUniqueIdProvider
+    {
+        private int _lastReturnedId = 0;
+
+        public int GetUniqueId()
+        {
+            return _lastReturnedId++;
+        }
+    }
+}
