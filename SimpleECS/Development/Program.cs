4e43a437e9c4e322965a2aa8bc4af011edb0c0ca
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-29 17:55:07 +0300

    Development project implementations

--- SimpleECS/Development/Program.cs
+++ SimpleECS/Development/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Development.Testing;
+using SimpleECS.Concretes;
 
 namespace Development
 {
     internal static class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var uniqueIdProvider = new UniqueIdProvider();
+            var componentCreator = new ComponentCreator();
+            var componentRepository = new ComponentRepository();
+            var entityCreator = new EntityCreator(uniqueIdProvider);
+            var entityTemplateInstantiator =
+                new EntityTemplateInstantiator(entityCreator, componentCreator, componentRepository);
+
+            var testRunner = new TestRunner(entityTemplateInstantiator);
+            testRunner.RunAll();
         }
     }
 }
---
+++ SimpleECS/Development/Testing/Performance/Instantiation/Test.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using SimpleECS.Interfaces;
+
+namespace Development.Testing.Performance.Instantiation
+{
+    internal class Test
+    {
+        private readonly IEntityTemplateInstantiator _instantiator;
+
+        public Test(IEntityTemplateInstantiator instantiator)
+        {
+            _instantiator = instantiator;
+        }
+
+        public Result Run(int entityCount, int componentPerEntity)
+        {
+            var templateCreationTime = MeasureEntityTemplateCreation(out var entityTemplate, componentPerEntity);
+            var instantiationTime = MeasureInstantiation(entityCount, entityTemplate);
+
+            return new Result(
+                entityCount: entityCount,
+                componentPerEntity: componentPerEntity,
+                templateCreationTime: templateCreationTime,
+                instantiationTime: instantiationTime);
+        }
+
+        private TimeSpan MeasureInstantiation(int entityCount, IEntityTemplate entityTemplate)
+        {
+            var sw = Stopwatch.StartNew();
+
+            for (var i = 0; i < entityCount; i++)
+            {
+                _instantiator.Instantiate(entityTemplate);
+            }
+
+            sw.Stop();
+            return sw.Elapsed;
+        }
+
+        private static TimeSpan MeasureEntityTemplateCreation(out EntityTemplate entityTemplate, int componentPerEntity)
+        {
+            var sw = Stopwatch.StartNew();
+
+            entityTemplate = new EntityTemplate(componentPerEntity);
+
+            sw.Stop();
+            return sw.Elapsed;
+        }
+    }
+}
