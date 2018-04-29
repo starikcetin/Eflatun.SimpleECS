d595cdc9898b0e3cf25db321279eaebacbf56843
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-29 13:59:56 +0300

    IEntityCreator implemented

---
+++ SimpleECS/SimpleECS/Concretes/EntityCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using SimpleECS.Interfaces;
+
+namespace SimpleECS.Concretes
+{
+    public class EntityCreator : IEntityCreator
+    {
+        private readonly IComponentCreator _componentCreator;
+
+        public EntityCreator(IComponentCreator componentCreator)
+        {
+            _componentCreator = componentCreator;
+        }
+
+        public IEnumerable<IComponent> CreateEntity(IEntityTemplate entityTemplate)
+        {
+            foreach (var componentType in entityTemplate.ComponentTypes)
+            {
+                yield return _componentCreator.CreateComponentFromType(componentType);
+            }
+        }
+    }
+}
