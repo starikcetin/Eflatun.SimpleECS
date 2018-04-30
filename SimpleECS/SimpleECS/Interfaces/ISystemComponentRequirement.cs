cf9fd83502ade180d40f90d74ef4b31b1f38ea33
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-30 17:59:30 +0300

    ISystemComponentRequirement interface

---
+++ SimpleECS/SimpleECS/Interfaces/ISystemComponentRequirement.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleECS.Interfaces
+{
+    public interface ISystemComponentRequirement
+    {
+        IReadOnlyCollection<Type> RequiredComponentTypes { get; }
+    }
+}
