3b5a5229d1f0c3633c5f04fda62ed01cd980e6dc
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-30 16:15:29 +0300

    SystemRepository class

---
+++ SimpleECS/SimpleECS/Concretes/SystemRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using SimpleECS.Interfaces;
+
+namespace SimpleECS.Concretes
+{
+    public class SystemRepository : ISystemRepository
+    {
+        private readonly HashSet<ISystem> _systems = new HashSet<ISystem>();
+        public IReadOnlyCollection<ISystem> RegisteredSystems => _systems;
+
+        public void Register(ISystem system)
+        {
+            _systems.Add(system);
+        }
+
+        public void Unregister(ISystem system)
+        {
+            _systems.Remove(system);
+        }
+    }
+}
