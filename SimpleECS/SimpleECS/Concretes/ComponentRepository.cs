5efa345725b1158c9714cc95020e276b4f5511e0
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-30 17:43:45 +0300

    Utilities for getting components of an entity in ComponentRepository

--- SimpleECS/SimpleECS/Concretes/ComponentRepository.cs
+++ SimpleECS/SimpleECS/Concretes/ComponentRepository.cs
@@ -1,17 +1,36 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using SimpleECS.Interfaces;
 
 namespace SimpleECS.Concretes
 {
     public class ComponentRepository : IComponentRepository
     {
-        private readonly Dictionary<IEntity, ISet<IComponent>> _entityComponents =
-            new Dictionary<IEntity, ISet<IComponent>>();
+        private readonly Dictionary<IEntity, HashSet<IComponent>> _entityComponents =
+            new Dictionary<IEntity, HashSet<IComponent>>();
 
         public void Register(IEntity entity, IComponent component)
         {
             EnsureKeyPresent(entity);
             _entityComponents[entity].Add(component);
+        }
+
+        public void Unregister(IEntity entity, IComponent component)
+        {
+            EnsureKeyPresent(entity);
+            _entityComponents[entity].Remove(component);
+        }
+
+        public IReadOnlyCollection<IComponent> GetComponents(IEntity entity)
+        {
+            EnsureKeyPresent(entity);
+            return _entityComponents[entity];
+        }
+
+        public T GetComponent<T>(IEntity entity) where T : IComponent
+        {
+            EnsureKeyPresent(entity);
+            return _entityComponents[entity].OfType<T>().Single();
         }
 
         private void EnsureKeyPresent(IEntity entity)
--- SimpleECS/SimpleECS/Interfaces/IComponentRepository.cs
+++ SimpleECS/SimpleECS/Interfaces/IComponentRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace SimpleECS.Interfaces
+﻿using System.Collections.Generic;
+
+namespace SimpleECS.Interfaces
 {
     public interface IComponentRepository
     {
         void Register(IEntity entity, IComponent component);
+        void Unregister(IEntity entity, IComponent component);
+        IReadOnlyCollection<IComponent> GetComponents(IEntity entity);
+        T GetComponent<T>(IEntity entity) where T : IComponent;
     }
 }
