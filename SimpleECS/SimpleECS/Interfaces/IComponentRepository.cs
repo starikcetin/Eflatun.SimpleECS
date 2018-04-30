db9ff9485a3351e9a9ce6ef5dfad8fbff32d0e20
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2018-04-30 18:01:31 +0300

    AllEntities and HasComponent in IComponentRepository interface and ComponentRepository class

--- SimpleECS/SimpleECS/Interfaces/IComponentRepository.cs
+++ SimpleECS/SimpleECS/Interfaces/IComponentRepository.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SimpleECS.Interfaces
 {
     public interface IComponentRepository
     {
+        IReadOnlyCollection<IEntity> AllEntities { get; }
         void Register(IEntity entity, IComponent component);
         void Unregister(IEntity entity, IComponent component);
         IReadOnlyCollection<IComponent> GetComponents(IEntity entity);
         T GetComponent<T>(IEntity entity) where T : IComponent;
+        bool HasComponent(IEntity entity, Type componentType);
     }
 }
